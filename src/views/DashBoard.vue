7fbe9718ed04c5e93fec5d7176970a238968da78
Nhan Le <lechinhan012@gmail.com>
2023-03-09 20:39:38 +0700

    Testing changes

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,6 +1,11 @@
 <template>
-  <div style="height: 1500px">
+  <div style="height: 1500px" class="container-fluid">
     <Navbar></Navbar>
+    <!-- <div style="color: white; padding-top: 50px">
+      <h2>Hello</h2>
+      <h5>{{ access_token }}</h5>
+      <button type="button" @click="getToken()">Get Token</button>
+    </div> -->
   </div>
 </template>
 
@@ -8,9 +13,16 @@
 import Navbar from '../components/HeaderNavbar.vue'
 export default {
   components: { Navbar },
-  setup() {},
-  methods: {},
-  mounted() {}
+  data() {
+    return {
+      access_token: null
+    }
+  },
+  methods: {
+    getToken() {
+      // this.access_token = this.$keycloak.token
+    }
+  }
 }
 </script>
 
