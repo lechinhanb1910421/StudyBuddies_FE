103fe6da2c6dbdd47ab625cd123eafccbbfb111d
Nhan Le <lechinhan012@gmail.com>
2023-03-11 10:37:23 +0700

    NewFeed page

--- src/components/NewFeed.vue
+++ src/components/NewFeed.vue
@@ -1,54 +1,65 @@
 <script>
-export default {}
+import UserPost from './UserPost.vue'
+export default {
+  components: { UserPost }
+}
 </script>
 <template lang="">
   <div class="container">
-    <div class="newPost_ctn">
-      <div class="newPost_ava">
-        <img src="../assets/Ganyu_2.jpeg" height="50" width="50" style="border-radius: 50%" alt="..." />
+    <div class="crePost_ctn">
+      <div class="crePost_ava">
+        <img src="../assets/Ganyu_2.jpeg" class="posts_ava" alt="..." />
       </div>
-      <div class="newPost_textarea">
+      <div class="crePost_input">
         <input type="text" class="form-control" placeholder="Hello Everett, how is your study?" />
       </div>
     </div>
-    <h1 style="color: white">Preserved for New Feed</h1>
+    <hr class="hr-white" />
+    <UserPost></UserPost>
   </div>
 </template>
 <style scoped>
-.newPost_ctn {
+hr.hr-white {
+  border-top: 1px solid white;
+}
+.crePost_ctn {
   margin-top: 15px;
   color: white;
   background-color: rgb(255 255 255 /0.1);
   display: flex;
   justify-content: center;
-  border-radius: 1rem;
-  padding-top: 10px;
-  padding-right: 15px;
-  padding-bottom: 20px;
+  border-radius: 0.75rem;
+  padding: 12px;
+  gap: 10px;
 }
-.newPost_ava {
+.crePost_ava {
   display: flex;
   justify-content: center;
   align-items: center;
-  flex: 1;
 }
-.newPost_textarea {
-  flex: 10;
+.posts_ava {
+  height: 40px;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
 }
-.newPost_textarea input {
-  height: 60px;
+.crePost_input {
+  flex-shrink: 0;
+  flex-grow: 1;
+}
+.crePost_input input {
+  height: 45px;
   background-color: rgb(255 255 255 /0.15);
   box-shadow: none;
   border: none;
-  font-size: 18px;
-  border-radius: 1rem;
+  font-size: 17px;
+  border-radius: 1.5rem;
 }
-.newPost_textarea input::placeholder {
+.crePost_input input::placeholder {
   color: white;
   opacity: 0.7;
 }
 .container {
-  padding-inline: 5%;
+  padding-inline: 12%;
   height: 300vh;
 }
 </style>
