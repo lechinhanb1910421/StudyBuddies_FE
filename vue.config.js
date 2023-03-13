46d4231b06afcaee476247d77805f0dac7bb11e8
Nhan Le <lechinhan012@gmail.com>
2023-03-13 20:44:25 +0700

    Remove unused proxy

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@', path.resolve(__dirname, './src'))
+  },
   devServer: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080/'
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:8080/nlcntt/api/'
+    //   }
+    // }
   }
 })
