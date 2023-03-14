acdc2248c763eb97838f793666bb929055280fc0
Nhan Le <lechinhan012@gmail.com>
2023-03-14 10:02:19 +0700

    Add new FriendBar

--- src/components/FriendBar.vue
+++ src/components/FriendBar.vue
@@ -1,13 +1,94 @@
-<template lang="">
-  <div class="preserve">
-    <h1 style="color: white">Preserved for Friend Bar</h1>
-  </div>
-</template>
 <script>
-export default {}
+import FriendProfileTile from '@/components/FriendProfileTile.vue'
+import GroupTile from '@/components/GroupTile.vue'
+export default {
+  components: {
+    FriendProfileTile,
+    GroupTile
+  },
+  data() {
+    return {
+      majorName: 'Infomation Technology'
+    }
+  }
+}
 </script>
+<template lang="">
+  <section class="interact_bar">
+    <div class="i_bar_titles">Groups</div>
+    <div class="groups">
+      <GroupTile></GroupTile>
+      <GroupTile></GroupTile>
+      <GroupTile></GroupTile>
+      <GroupTile></GroupTile>
+      <GroupTile></GroupTile>
+      <GroupTile></GroupTile>
+      <GroupTile></GroupTile>
+      <GroupTile></GroupTile>
+    </div>
+    <hr class="i_bar_hr" />
+    <div class="i_bar_titles">Friends</div>
+    <div class="friends">
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+      <FriendProfileTile></FriendProfileTile>
+    </div>
+  </section>
+</template>
 <style scoped>
-.preserve {
-  background-color: rgb(255 255 255 / 0.2);
+.interact_bar {
+  width: 22%;
+  position: fixed;
+  top: 60px;
+  right: 0;
+  height: 100%;
+  background-color: #181a1d;
+  /* border-left: 1px solid rgb(128 128 128 / 0.5); */
+  height: 100vh;
+}
+.i_bar_hr {
+  opacity: 0.3;
+  margin: 0;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 0;
+}
+.interact_bar {
+  color: white !important;
+  padding: 10px;
+}
+.groups {
+  height: 30%;
+}
+.friends {
+  /* min-height: 45%; */
+  height: 50%;
+}
+.groups,
+.friends {
+  padding-inline-start: 10px;
+  padding-inline-end: 5px;
+  overflow-y: scroll;
+}
+.groups::-webkit-scrollbar {
+  display: none;
+}
+.friends::-webkit-scrollbar {
+  display: none;
+}
+.i_bar_titles {
+  padding-inline-start: 15px;
+  padding-bottom: 10px;
+  padding-top: 5px;
+  font-size: 20px;
+  font-weight: 600;
 }
 </style>
