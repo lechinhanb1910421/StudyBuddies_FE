56c81d5385e588340772aab859d492fa83887ee4
Nhan Le <lechinhan012@gmail.com>
2023-03-15 20:57:14 +0700

    Minor changes

--- src/main.js
+++ src/main.js
@@ -10,19 +10,24 @@ import 'bootstrap/dist/js/bootstrap.bundle'
 //   realm: process.env.VUE_APP_KEYCLOAK_REALM,
 //   clientId: process.env.VUE_APP_KEYCLOAK_CLIENT_ID,
 //   onLoad: 'login-required',
-//   checkLoginIframe: false,
+//   checkLoginIframe: true,
 //   flow: 'standard'
 // }
+// const myApp = createApp(App).use(router)
 // let keycloak = new Keycloak(initOptions)
 // keycloak
-//   .init({ onLoad: initOptions.onLoad })
+//   .init({ onLoad: initOptions.onLoad, flow: initOptions.flow })
 //   .then((auth) => {
 //     if (!auth) {
 //       window.location.reload()
+//       myApp.mount('#app')
+//     } else {
+//       myApp.config.globalProperties.$keycloak = keycloak
+//       myApp.mount('#app')
 //     }
 //     setInterval(() => {
 //       keycloak
-//         .updateToken(25)
+//         .updateToken(255)
 //         .then((refreshed) => {
 //           if (refreshed) {
 //             console.log('Token refreshed ' + refreshed)
@@ -35,11 +40,11 @@ import 'bootstrap/dist/js/bootstrap.bundle'
 //         .catch(() => {
 //           console.log('Failed to refresh token')
 //         })
-//     }, 25000)
+//     }, 255000)
 //   })
 //   .catch(() => {
 //     console.log('Authenticated Failed')
 //   })
-const myApp = createApp(App)
-// myApp.config.globalProperties.$keycloak = keycloak
-myApp.use(router).mount('#app')
+
+const myApp = createApp(App).use(router)
+myApp.mount('#app')
