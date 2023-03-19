6cda7564f3ffb1a6a60da7b2551aa678575205bc
Nhan Le <lechinhan012@gmail.com>
2023-03-19 10:33:52 +0700

    New components

---
+++ src/components/SearchMenuBar.vue
@@ -0,0 +1,59 @@
+<script>
+import MenuTile from './MenuTile.vue'
+export default {
+  components: { MenuTile },
+  data() {
+    return {
+      filters: ['Post', 'User', 'School', 'Major', 'Topic']
+    }
+  }
+}
+</script>
+<template>
+  <section class="menu_container">
+    <div class="search_title_ctn">
+      <i class="fas fa-search menu_icon"></i>
+      <span class="search_title">Search result</span>
+    </div>
+    <hr class="menu_hr" />
+    <div class="filters_ctn" v-if="this.filters">
+      <div v-for="(title, index) in this.filters" :key="index">
+        <MenuTile :titleName="title"></MenuTile>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped>
+.menu_hr {
+  margin-block: 7px;
+}
+.menu_container {
+  width: 23%;
+  height: 100vh;
+  color: white;
+  padding: 10px;
+  position: fixed;
+  left: 0;
+  border-right: 1px solid rgb(255 255 255 /0.2);
+}
+.search_title_ctn {
+  height: 50px;
+  padding-inline-start: 20px;
+  display: flex;
+  gap: 12px;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 24px;
+  font-weight: 600;
+}
+.menu_icon {
+  font-size: 24px;
+}
+.filters_ctn {
+  padding-inline: 15px;
+  font-size: 20px;
+}
+.filters_ctn * {
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/views/SearchResults.vue
@@ -0,0 +1,33 @@
+<script>
+import SearchMenuBar from '@/components/SearchMenuBar.vue'
+export default {
+  components: { SearchMenuBar },
+  props: ['sQuery']
+}
+</script>
+<template>
+  <div class="container-fluid">
+    <SearchMenuBar></SearchMenuBar>
+    <div class="mainCol">
+      <h2>this is main col</h2>
+      <h2>Query: {{ sQuery }}</h2>
+    </div>
+  </div>
+</template>
+<style scoped>
+.container-fluid {
+  padding-inline: 0;
+  background-color: #181a1d;
+  padding-top: 61px;
+  display: block;
+  min-height: 100vh;
+  display: flex;
+  border-right: 1px solid white;
+}
+.mainCol {
+  color: white;
+  margin-left: 23%;
+  /* background-color: rgb(255 255 255 /0.3); */
+  flex-grow: 1;
+}
+</style>
