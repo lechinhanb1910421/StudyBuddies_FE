164f09109b97729dd323b6d0fe1f0b5435f3e073
Nhan Le <lechinhan012@gmail.com>
2023-03-20 15:53:00 +0700

    Remove fixed config for Firebase

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDnSCgq4HcfNR4lgtQaqqaWLW8ZovBFZSk',
-  authDomain: 'study-buddies-8b16e.firebaseapp.com',
-  projectId: 'study-buddies-8b16e',
-  storageBucket: 'study-buddies-8b16e.appspot.com',
-  messagingSenderId: '531865224994',
-  appId: '1:531865224994:web:507a76e3c7abfd5efb077b'
+  apiKey: process.env.VUE_APP_FB_APIKEY,
+  authDomain: process.env.VUE_APP_FB_AUTHDOMAIN,
+  projectId: process.env.VUE_APP_FB_PROJECTID,
+  storageBucket: process.env.VUE_APP_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FB_APPID
 }
 
 const app = initializeApp(firebaseConfig)
