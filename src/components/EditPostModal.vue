de6df14e8e03b847482847b29c49fd39041849f9
Nhan Le <lechinhan012@gmail.com>
2023-03-25 20:49:53 +0700

    Refine update Post

--- src/components/EditPostModal.vue
+++ src/components/EditPostModal.vue
@@ -6,6 +6,7 @@ import { loggedInUserStorage } from '@/stores/loggedInUser'
 import ToastService from '@/services/toast.service'
 
 export default {
+  emits: ['postEdited'],
   props: ['oldPost'],
   setup() {
     const userStore = loggedInUserStorage()
@@ -22,17 +23,32 @@ export default {
       postMajor: 1,
       postImageUrl: '',
       loginName: '',
-      newInputImage: false
+      newInputImage: false,
+      majors: {
+        General: 1,
+        'Information Technology': 2,
+        'Information System': 3,
+        'Computer Science': 4,
+        'Software Engineering': 5,
+        'Computer Network and Data Communication': 6,
+        'Multimedia Communication': 7
+      },
+      topics: { General: 1, Recreation: 2, 'Knowleage Share': 3, Experience: 4, Help: 5, Sale: 6 }
     }
   },
   methods: {
-    setOldData() {
+    setEditedData() {
       this.post_content = this.oldPost.content
-      if (this.oldPost.picUrls[0] != undefined) {
+      if (this.oldPost.picUrls[0].length > 0) {
         this.newImagePreview = this.oldPost.picUrls[0]
         this.hasImage = true
+      } else {
+        this.newImagePreview = ''
+        this.newImageData = ''
+        this.hasImage = false
       }
-      this.resize_textarea()
+      this.postTopic = this.topics[this.oldPost.topicName]
+      this.postMajor = this.majors[this.oldPost.majorName]
     },
     getUserLoginName() {
       this.loginName = this.userStore.user.loginName
@@ -42,16 +58,26 @@ export default {
         content: this.post_content,
         topicId: this.postTopic,
         majorId: this.postMajor,
-        imageUrl: this.newInputImage ? this.postImageUrl : this.oldPost.picUrls[0]
+        imageUrl: this.oldPost.picUrls[0]
       }
-      await PostService.updatePost(this.$keycloak.token, payload, this.oldPost.postId)
+      if (this.newInputImage) {
+        payload.imageUrl = this.postImageUrl
+      }
+      if (!this.hasImage) {
+        payload.imageUrl = ''
+      }
       this.resetAddPostModal()
       this.removePreviewImage()
-      setTimeout(() => {
-        ToastService.showPostUpdateToast()
-      }, 700)
+      const data = await PostService.updatePost(this.$keycloak.token, payload, this.oldPost.postId)
+      if (data.message == 'Post was updated successfully') {
+        this.$emit('postEdited')
+        setTimeout(() => {
+          ToastService.showPostUpdateToast()
+        }, 127)
+      }
     },
     async submitEditPostForm() {
+      ToastService.showPostProcessingToast()
       this.$refs.closeModal.click()
       if (this.newInputImage) {
         await this.uploadImage(this.userLoginName)
@@ -61,7 +87,7 @@ export default {
     },
     resize_textarea() {
       const { textarea } = this.$refs
-      var newHeight = textarea.scrollHeight
+      var newHeight = textarea.scrollHeight ?? 60
       if (newHeight > 260) {
         newHeight = 260
       }
@@ -69,11 +95,9 @@ export default {
       textarea.style.height = newHeight - 4 + 'px'
     },
     async setPreviewImage(event) {
-      this.newImageData = ''
       this.newImageData = event.target.files[0]
       const reader = new FileReader()
       reader.readAsDataURL(this.newImageData)
-      this.newImagePreview = ''
       reader.onload = (e) => {
         this.newImagePreview = e.target.result
       }
@@ -82,7 +106,8 @@ export default {
     },
     async removePreviewImage() {
       this.newImagePreview = ''
-      // this.$refs.add_img_input.value = null
+      this.newImageData = ''
+      // this.$refs.add_img_input.files[0] = ''
       this.hasImage = false
     },
     async uploadImage() {
@@ -138,86 +163,82 @@ export default {
     }
   },
   mounted() {
-    this.setOldData()
+    this.setEditedData()
     this.resize_textarea()
   }
 }
 </script>
 <template>
-  <div class="modal modal-lg fade" id="editPostModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header text-center">
-          <h1 class="modal-title fs-5 w-100" id="modal_title">Editing your post</h1>
-          <button type="button" ref="closeModal" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div style="flex-grow: 1; flex: 1">
-            <div class="options mb-2">
-              <div class="opts_topic">
-                <label for="topic">Topic: </label>
-                <select class="form-select post_opts" @change="setPostTopic($event)" id="topic" aria-label="Select post topic">
-                  <option value="1" selected>General</option>
-                  <option value="2">Recreation</option>
-                  <option value="3">Knowleage Share</option>
-                  <option value="4">Experience</option>
-                  <option value="5">Help</option>
-                  <option value="6">Sale</option>
-                </select>
-              </div>
-              <div class="opts_major">
-                <label for="major">Major: </label>
-                <select class="form-select post_opts" @change="setPostMajor($event)" id="major" aria-label="Select post Major">
-                  <option value="1" selected>All Majors</option>
-                  <option value="2">Information Technology</option>
-                  <option value="3">Information System</option>
-                  <option value="4">Computer Science</option>
-                  <option value="5">Software Engineering</option>
-                  <option value="6">Computer Network and Data Communication</option>
-                  <option value="7">Multimedia Communication</option>
-                </select>
-              </div>
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h1 class="modal-title fs-5 w-100" id="modal_title">Edit your post</h1>
+        <button type="button" ref="closeModal" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div style="flex-grow: 1; flex: 1">
+          <div class="options mb-2">
+            <div class="opts_topic">
+              <label for="topic">Topic: </label>
+              <select class="form-select post_opts" :value="this.postTopic" @change="setPostTopic($event)" id="topic" aria-label="Select post topic">
+                <option value="1" selected>General</option>
+                <option value="2">Recreation</option>
+                <option value="3">Knowleage Share</option>
+                <option value="4">Experience</option>
+                <option value="5">Help</option>
+                <option value="6">Sale</option>
+              </select>
             </div>
-            <div class="content mb-3">
-              <textarea
-                class="form-control content_textarea"
-                rows="1"
-                maxlength="5000"
-                ref="textarea"
-                :class="{ post_text_lg: this.post_content_lg }"
-                v-model="this.post_content"
-                @focus="resize_textarea"
-                @keyup="resize_textarea">
-              </textarea>
+            <div class="opts_major">
+              <label for="major">Major: </label>
+              <select class="form-select post_opts" :value="this.postMajor" @change="setPostMajor($event)" id="major" aria-label="Select post Major">
+                <option value="1" selected>All Majors</option>
+                <option value="2">Information Technology</option>
+                <option value="3">Information System</option>
+                <option value="4">Computer Science</option>
+                <option value="5">Software Engineering</option>
+                <option value="6">Computer Network and Data Communication</option>
+                <option value="7">Multimedia Communication</option>
+              </select>
             </div>
-            <img v-if="this.hasImage" class="add_pic_pre_image" :src="newImagePreview" alt="..." />
           </div>
-          <div class="add_pic_ctrs">
-            <button type="button" class="btn btn-danger add_pic_del_btn" @click="removePreviewImage" v-if="this.hasImage">Delete Image</button>
-            <label for="add_img_input" class="add_pic" v-if="!this.hasImage">
-              <i class="fas fa-images"></i>
-              <span>Add Image</span>
-              <input
-                type="file"
-                name="photo"
-                ref="add_img_input"
-                id="add_img_input"
-                @change="setPreviewImage"
-                accept="image/png, image/gif, image/jpeg" />
-            </label>
+          <div class="content mb-3">
+            <textarea
+              class="form-control content_textarea"
+              rows="1"
+              maxlength="5000"
+              ref="textarea"
+              :class="{ post_text_lg: this.post_content_lg }"
+              v-model="this.post_content"
+              @focus="resize_textarea"
+              @keyup="resize_textarea">
+            </textarea>
           </div>
+          <img v-if="this.hasImage" class="add_pic_pre_image" :src="this.newImagePreview" alt="..." />
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary btn_add_post" @click="submitEditPostForm">Post</button>
+        <div class="add_pic_ctrs">
+          <button type="button" class="btn btn-danger add_pic_del_btn" @click="removePreviewImage" v-if="this.hasImage">Delete Image</button>
+          <label :for="'edit_img_input' + this.oldPost.postId" class="add_pic" v-if="!this.hasImage">
+            <i class="fas fa-images"></i>
+            <span>Add Image</span>
+            <input
+              type="file"
+              name="photo"
+              :ref="'edit_img_input' + this.oldPost.postId"
+              :id="'edit_img_input' + this.oldPost.postId"
+              @change="setPreviewImage"
+              style="display: none"
+              accept="image/png, image/gif, image/jpeg" />
+          </label>
         </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary btn_add_post" @click="submitEditPostForm">Edit post</button>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-#add_img_input {
-  display: none;
-}
 .add_pic_ctrs {
   margin-top: 10px;
   margin-inline: auto;
@@ -264,7 +285,7 @@ export default {
   height: 30px;
 }
 .modal-body {
-  height: 400px;
+  height: 550px;
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
